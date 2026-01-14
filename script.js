0fa2c652bd5bc42259b0b56b733859d219e7ca97
bobgobbar <chilltimen00b@gmail.com>
2026-01-14 21:46:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let clicks = 0;
+
+const trigger = document.getElementById("secret-trigger");
+const egg = document.getElementById("easter-egg");
+
+if (trigger) {
+  trigger.addEventListener("click", () => {
+    clicks++;
+
+    if (clicks === 5) {
+      egg.style.display = "block";
+      egg.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+}
