d745c8a50433fcf1db5be90b5a90cc37d7b30f7a
bobgobbar <chilltimen00b@gmail.com>
2026-01-29 17:00:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,3 @@
-let clicks = 0;
-
-const trigger = document.getElementById("secret-trigger");
-const egg = document.getElementById("easter-egg");
-
-if (trigger) {
-  trigger.addEventListener("click", () => {
-    clicks++;
-
-    if (clicks === 5) {
-      egg.style.display = "block";
-      egg.scrollIntoView({ behavior: "smooth" });
-    }
-  });
-}
-
-const music = document.getElementById("bg-music");
-music.volume = 0.2;
-
-const music = document.getElementById("bg-music");
-const icon = document.getElementById("music-icon");
-
-music.volume = 0.2;
-
-function toggleMusic() {
-  if (music.paused) {
-    music.play();
-    icon.textContent = "⏸";
-  } else {
-    music.pause();
-    icon.textContent = "▶";
-  }
-}
-
 let isPlaying = false;
 
 function toggleMusic() {
@@ -42,13 +8,12 @@ function toggleMusic() {
   if (!isPlaying) {
     music.play();
     icon.textContent = "⏸";
-    text.textContent = "haha music wrwmr";
+    text.textContent = "haha music wrmwr";
     isPlaying = true;
   } else {
     music.pause();
     icon.textContent = "▶";
-    text.textContent = "this button looks cool";
+    text.textContent = "start the vibe";
     isPlaying = false;
   }
 }
-
